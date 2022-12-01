696936327432ba6f9bb1b356f02531a4a31229cc
Douglas Cananéa <cananeadouglas@gmail.com>
2022-12-01 20:03:50 -0300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from flask_script._compat import text_type
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 # chamando o arquivo defatul dentro de controllers, nosso Hello World
 from app.controllers import default
